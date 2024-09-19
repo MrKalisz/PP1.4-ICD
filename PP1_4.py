2947946ed5d423bcbf282d843e7a1dd3e3d0cff5
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-18 22:00:18 -0400

    Update PP1_4.py

--- PP1_4.py
+++ PP1_4.py
@@ -1,3 +1,20 @@
 
 
-#Write Assignment code here
+def q1():
+  #Write Assignment code here
+
+def q2():
+  #Write Assignment code here
+
+def q3():
+  #Write Assignment code here
+
+def q4():
+  #Write Assignment code here
+
+#Do not edit code below this comment
+
+q1()
+q2()
+q3()
+q4()
